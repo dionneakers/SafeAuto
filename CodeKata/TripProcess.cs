a31697b5d149f680a5dcf7a47bbc5ab882f66c82
dionneakers <66050956+dionneakers@users.noreply.github.com>
2020-06-05 12:30:50 -0400

    Update TripProcess.cs

--- CodeKata/TripProcess.cs
+++ CodeKata/TripProcess.cs
@@ -14,7 +14,7 @@ namespace CodeKata
                 throw new ArgumentException("File must exist and have a length greater than zero", nameof(file));
             }
             
-            https://docs.microsoft.com/en-us/dotnet/api/system.io.file.openread?view=netcore-3.1
+            //https:docs.microsoft.com/en-us/dotnet/api/system.io.file.openread?view=netcore-3.1
             var fs = new FileStream(fileInfo.FullName, FileMode.Open)
             {
                 return Process(fs);
