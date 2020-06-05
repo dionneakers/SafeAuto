a6395c6bc46cbddf9feaa93e6227562fa6c7ebe9
dionneakers <66050956+dionneakers@users.noreply.github.com>
2020-06-05 12:24:23 -0400

    Create TripProcess.cs

---
+++ CodeKata/TripProcess.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using log4net;
+
+namespace CodeKata
+{
+    public class TripProcess{
+
+        public TripResult Process(string file)
+        {
+            var fileInfo = new FileInfo(file); // throws ArgumentNullException
+            if (fileInfo.Exists == false || fileInfo.Length == 0)
+            {
+                throw new ArgumentException("File must exist and have a length greater than zero", nameof(file));
+            }
+            
+            https://docs.microsoft.com/en-us/dotnet/api/system.io.file.openread?view=netcore-3.1
+            var fs = new FileStream(fileInfo.FullName, FileMode.Open)
+            {
+                return Process(fs);
+            }
+        }
+    }
+}
